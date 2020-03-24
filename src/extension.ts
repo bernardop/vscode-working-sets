a75b936a84ef6b931ca4efa0cb7278edad711aa9
Bernardo Pacheco <berna.ipach@gmail.com>
2020-03-24 12:01:18 -0400

    Update prettier config

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { WorkingSetsProvider } from "./workingSets";
+import * as vscode from "vscode"
+import { WorkingSetsProvider } from "./workingSets"
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const workingSetsProvider = new WorkingSetsProvider(context);
-  vscode.window.registerTreeDataProvider("workingSets", workingSetsProvider);
+  const workingSetsProvider = new WorkingSetsProvider(context)
+  vscode.window.registerTreeDataProvider("workingSets", workingSetsProvider)
 
   context.subscriptions.push(
     vscode.commands.registerCommand("workingSets.create", () =>
       workingSetsProvider.create()
     ),
-    vscode.commands.registerCommand("workingSets.delete", workingSet =>
+    vscode.commands.registerCommand("workingSets.delete", (workingSet) =>
       workingSetsProvider.delete(workingSet)
     )
-  );
+  )
 }
 
 // this method is called when your extension is deactivated
--- src/workingSets.ts
+++ src/workingSets.ts
@@ -1,78 +1,78 @@
-import * as vscode from "vscode";
+import * as vscode from "vscode"
 
 export class WorkingSetsProvider
   implements vscode.TreeDataProvider<vscode.TreeItem> {
   private _onDidChangeTreeData: vscode.EventEmitter<
     vscode.TreeItem | undefined
-  > = new vscode.EventEmitter<vscode.TreeItem | undefined>();
+  > = new vscode.EventEmitter<vscode.TreeItem | undefined>()
   readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | undefined> = this
-    ._onDidChangeTreeData.event;
+    ._onDidChangeTreeData.event
 
-  private workspaceWorkingSets: Record<string, string[]> = {};
+  private workspaceWorkingSets: Record<string, string[]> = {}
 
   constructor(private readonly context: vscode.ExtensionContext) {
     const storedWorkingSets:
       | Record<string, string[]>
-      | undefined = context.workspaceState.get("workingSets");
+      | undefined = context.workspaceState.get("workingSets")
 
     if (storedWorkingSets) {
-      this.workspaceWorkingSets = storedWorkingSets;
+      this.workspaceWorkingSets = storedWorkingSets
     } else {
       context.workspaceState.update("workingSets", {}).then(
         () => {
-          this.workspaceWorkingSets = {};
+          this.workspaceWorkingSets = {}
         },
         () => {
           vscode.window.showErrorMessage(
             "There was a problem initializing the extension"
-          );
+          )
         }
-      );
+      )
     }
   }
 
   getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
-    return element;
+    return element
   }
 
   getChildren(): vscode.TreeItem[] {
     return Object.keys(this.workspaceWorkingSets).map(
-      name => new WorkingSet(name, vscode.TreeItemCollapsibleState.Collapsed)
-    );
+      (name) => new WorkingSet(name, vscode.TreeItemCollapsibleState.Collapsed)
+    )
   }
 
   async create() {
     const name = await vscode.window.showInputBox({
-      prompt: "New working set name"
-    });
+      prompt: "New working set name",
+    })
     if (name) {
       if (this.workingSetExists(name)) {
         vscode.window.showInformationMessage(
           "A working set with that name already exists"
-        );
+        )
       } else {
-        this.workspaceWorkingSets[name] = [];
+        this.workspaceWorkingSets[name] = []
         vscode.window.showInformationMessage(
           `"${name}" working set successfully created`
-        );
-        this._onDidChangeTreeData.fire();
+        )
+        this._onDidChangeTreeData.fire()
       }
     }
   }
 
   delete(workingSet: WorkingSet | undefined): void {
     if (workingSet) {
-      const name = workingSet.label;
-      delete this.workspaceWorkingSets[name];
+      const name = workingSet.label
+      delete this.workspaceWorkingSets[name]
       vscode.window.showInformationMessage(
         `"${name}" working set successfully deleted`
-      );
-      this._onDidChangeTreeData.fire();
+      )
+      this._onDidChangeTreeData.fire()
     }
   }
 
   private workingSetExists(name: string): boolean {
-    return Object.keys(this.workspaceWorkingSets).includes(name);
+    return Object.keys(this.workspaceWorkingSets).includes(name)
   }
 }
 
@@ -81,8 +81,8 @@ class WorkingSet extends vscode.TreeItem {
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState
   ) {
-    super(label, collapsibleState);
+    super(label, collapsibleState)
   }
 
-  contextValue = "workingSet";
+  contextValue = "workingSet"
 }
